548cd1bfa0e7df51443f3316d9115b91986af55e
feranstirman1 <00024117@uca.edu.sv>
2018-11-09 11:09:35 -0600

    completado

---
+++ controllers/UserController.js
@@ -0,0 +1,89 @@
+const mongoose= require('mongoose');
+const User= require('../models/users');
+const AuthController={};
+const bcrypt= require('bcrypt');
+
+AuthController.login= function(req,res,next){
+    res.render('signin');
+}
+
+AuthController.create= function(req,res,next){
+    res.render('signup');
+}
+
+AuthController.store= async function(req,res){
+    let user= {
+        email:req.body.email,
+        password: req.body.password
+    }
+    await User.create(user,(error,user)=>{
+        if(error){
+            return res.render('signup',{err:error,email:user.email});
+        }else{
+            let data={
+                userId: user._id.toString(),
+                email:user.email,
+                password: user.password
+            }
+
+            bcrypt.hash(data.userId,10,function(err,hash){
+                if(err){
+                    next(err);
+                }
+                
+                data.userId=hash;
+
+                req.session.user= JSON.stringify(data);
+                console.log(req.session.user);
+                return res.redirect('/users/profile');
+            });
+        }
+    })
+};
+
+AuthController.profile= function(req,res){
+    return res.render('profile');
+}
+
+AuthController.signin = function (req, res,next) {
+    var data = {};
+    //user autentication es el metodo que nos permitira ingresar al sistema
+    User.authenticate(req.body.email, req.body.password, (error, user) => {
+        if (error || !user) {
+            res.render('signin', { err: error, email: req.body.email });
+            //return res.send("Ddd");
+        }
+        else {
+                data.userId= user._id.toString(),
+                data.email= user.email,
+                data.password=user.password
+            
+            //este método nos encriptara el userId para que sea alamcenado en la sesion
+            bcrypt.hash(data.userId, 10, function (err, hash) {
+                if (err) {
+                    next(err);
+                }
+                data.userId = hash;
+                //parseamos el objeto a cadena
+                req.session.user = JSON.stringify(data);
+                //si es correcto nos dirigira al perfil del usuario que esta ingresando.
+                return res.redirect('/users/profile');
+            });
+
+        }
+    });
+};
+
+AuthController.logout= function(req,res,next){
+    if(req.session){
+        req.session.destroy(function(err){
+            if(err){
+                next(err);
+            }else{
+                res.redirect('/');
+            }
+        });
+    }
+}
+
+module.exports= AuthController;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
-var express = require('express');
-var router = express.Router();
+const express= require('express');
+const router= express.Router();
+
+const AuthController=require("../controllers/UserController");
+const AuthMiddleware= require("../middlewares/AuthMIddleware");
+const user= require("../models/users");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/signup',AuthController.create);
+//ruta que enviara los datos del usuario para almacenarlos en la base de datos
+router.post('/signup',AuthController.store);
+//ruta que nos devolvera el formulario para ingresar
+router.get('/signin',AuthController.login);
+//ruta que enviara los datos del usuario para ingresar al sistema
+router.post('/signin',AuthController.signin);
+//ruta para salir del sistema
+router.get('/logout',AuthController.logout);
+/*Middlewar que verifica que solo los usuarios registrados podran ingresar a esta seccion */
+router.use(AuthMiddleware.isAuthentication);
+//ruta para acceder al perifl
+router.get('/profile',AuthController.profile);
 
 module.exports = router;
